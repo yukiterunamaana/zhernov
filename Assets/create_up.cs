1d6b1004ed62fc3c6cccd29475547f6e28affb6c
jardenooob <118860501+jardenooob@users.noreply.github.com>
2025-03-14 13:09:20 +0300

    scroll

---
+++ Assets/create_up.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_up : MonoBehaviour
+{
+    public itm_scr tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/itm_scr.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itm_scr : MonoBehaviour
+{
+    public TMP_Text name;
+    public TMP_Text dis;
+    public TMP_Text pr;
+    public Image pic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
